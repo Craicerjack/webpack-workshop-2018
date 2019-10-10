5006b20faf46db0e01aace4298d9d83303c7443b
Carlos Tighe <carlos.tighe@insight-centre.org>
2019-10-10 16:12:48 +0100

    commonjs default export

---
+++ src/button.js
@@ -0,0 +1,8 @@
+// take a str, the button label and return an element
+// commonjs 
+// react uses commonjs syntax
+
+// default export   
+module.exports = (buttonName) => {
+  return `Button: ${buttonName}`;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import nav from "./nav";
 import {top, bottom} from "./footer";
+//can do this because webpack has middleware to handle it but normally cant have es module syntax and commonjs syntax
+import makeButton from "./button"; 
+
 
 console.log(nav()); 
 console.log("top: ", top, " and bottom: ", bottom);
